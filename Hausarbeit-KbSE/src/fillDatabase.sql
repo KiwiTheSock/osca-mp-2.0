1bd9b1f8e8ec8fa8162eb09470364d5d71ed895d
pmarkman <pmarkman@SI0002-20.intra.edvsz>
2020-02-06 19:22:20 +0100

    blabla

---
+++ Hausarbeit-KbSE/src/fillDatabase.sql
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  pmarkman
+ * Created: 01.02.2020
+ */
+
+INSERT INTO Exam (ID, BEGINN, DATUM, DURATION, FINISH, SPACEFORSTUDENTS, DEPARTMENT_ID)
+VALUES (3,'14:00','2020-02-03', 30, '14:30', 1, 5);
+INSERT INTO Exam (ID, BEGINN, DATUM, DURATION, FINISH, SPACEFORSTUDENTS, DEPARTMENT_ID)
+VALUES (2,'13:00','2020-02-03', 30, '13:30', 1, 5);
